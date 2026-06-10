6e4a13d3844ecc587ac3423c254b7554903153cc
Xansiety <ferando.code@gmail.com>
2026-06-09 22:29:46 -0600

    Refactor PlanetList to implement optimistic updates and enhance planet name formatting

--- src/actions/update-planet.action.ts
+++ src/actions/update-planet.action.ts
@@ -2,8 +2,16 @@ import { planetsApi } from "../api/planetsApi";
 import { Planet } from "../interfaces/planet.interface";
 import { logErrorToMyService } from "../shared/logger";
 
+const sleep = async () => {
+  return new Promise((r) => setTimeout(r, 2000));
+};
+
 export const updatePlanetAction = async (planet: Planet) => {
   try {
+    await sleep(); // 2 seconds
+    throw new Error(
+      "Error al actualizar el planeta (PRUEBA handle error in Transition)",
+    );
     const response = await planetsApi.patch<Planet>(`/${planet.id}`, planet);
     console.log("Planet updated successfully");
     return response.data;
--- src/pages/ui/PlanetList.tsx
+++ src/pages/ui/PlanetList.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useOptimistic, useTransition } from "react";
 import { updatePlanetAction } from "../../actions/update-planet.action";
 import { Planet } from "../../interfaces/planet.interface";
 
@@ -7,18 +7,34 @@ interface Props {
 }
 
 export const PlanetList = ({ planets }: Props) => {
-  const [newPlanets, setNewPlanets] = useState<Planet[]>(planets);
+  const [_isPending, startTransition] = useTransition(); // useTransition hook: this allow to keep the UI in sync with the state
+
+  // What is Optimistic Update in React
+  // To change the state optimistically, we can use the useOptimistic hook
+  const [optimisticPlanets, setOptimisticPlanets] = useOptimistic(
+    planets,
+    (currentPlanets: Planet[], newPlanet: Planet) => {
+      return currentPlanets.map((p) => (p.id === newPlanet.id ? newPlanet : p));
+    },
+  );
   const handleUpdatePlanet = async (planet: Planet) => {
-    planet.name = planet.name.toUpperCase();
-    const updatePlanet = await updatePlanetAction(planet);
-    setNewPlanets((planets) => {
-      return planets.map((p) => (p.id === updatePlanet.id ? updatePlanet : p));
+    const data = {
+      ...planet,
+      name: planet.name.toUpperCase(),
+    };
+    startTransition(async () => {
+      try {
+        setOptimisticPlanets(data); // Optimistic - should be called before the action
+        await updatePlanetAction(data);
+      } catch (error) {
+        setOptimisticPlanets(planet); // If the action fails, reset the optimistic state with the previous value
+      }
     });
   };
 
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 animate-fadeIn">
-      {newPlanets.map((planet) => (
+      {optimisticPlanets.map((planet) => (
         <div key={planet.id} className="p-4 bg-gray-100 rounded shadow">
           <h2 className="text-xl font-semibold">{planet.name}</h2>
           <p className="text-gray-700">{planet.type}</p>
